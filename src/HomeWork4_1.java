91084837d27b2156bbb27dc97da0c75a6296b7c3
Hiro-1978 <hiromiuki11@gmail.com>
2021-04-23 13:29:39 +0700

    JavaCTF02
    Home work 4.1-4.2

---
+++ src/HomeWork4_1.java
@@ -0,0 +1,27 @@
+
+public class HomeWork4_1 {
+
+    public static void main(String[] args) {
+        // รวมข้อ 4.1-4.2
+        int score = 20;
+        boolean IsHandSome = true;
+        
+        if(score>=80){
+            if(score>=80){
+            System.out.println("Good");
+            }else if(IsHandSome==true){
+            System.out.println("Good");
+            }
+        } else if(score>=50 && score<80){
+            if(score>=50 && score<80){
+            System.out.println("normal");
+            }else if(IsHandSome==true){
+            System.out.println("Good");
+            }
+        } else if(IsHandSome==true){
+            System.out.println("Good");
+        }else{
+            System.out.println("fail");
+        }
+    }
+}
